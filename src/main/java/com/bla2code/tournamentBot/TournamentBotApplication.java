ace09b2d92f94d8ee655dc3df15ecf60eaa94b62
Alexey Muchkaev <alexey.muchkaev@corp.codeinside.ru>
2021-01-07 22:01:06 +0300

    feature/1 application structure

---
+++ src/main/java/com/bla2code/tournamentBot/TournamentBotApplication.java
@@ -0,0 +1,13 @@
+package com.bla2code.tournamentBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentBotApplication.class, args);
+	}
+
+}
